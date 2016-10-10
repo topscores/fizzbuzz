82e1adf261395ef2bf3bd87191e6460f3a845654
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-10-10 10:19:23 +0700

    Restore stubs state after use

--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import sinon from 'sinon'
 import { isFizz } from '../src/fizz'
 import * as fizz from '../src/fizz'
+import * as buzz from '../src/buzz'
 import { fizzbuzz } from '../src/fizzbuzz'
 describe('isFizz', () => {
   it('Should return true if number % 3 equal 0', () => {
@@ -17,12 +18,18 @@ describe('isFizz', () => {
 describe('fizzbuzz', () => {
   it('Should return fizzbuzz if number % 15 equal 0', () => {
     sinon.stub(fizz, 'isFizz').returns(true)
+    sinon.stub(buzz, 'isBuzz').returns(true)
     expect(fizzbuzz(15)).to.equal('fizzbuzz')
     expect(fizzbuzz(45)).to.equal('fizzbuzz')
     fizz.isFizz.restore()
+    buzz.isBuzz.restore()
   })
   it('Should return fizz if number % 3 equal 0', () => {
+    sinon.stub(fizz, 'isFizz').returns(true)
+    sinon.stub(buzz, 'isBuzz').returns(false)
     expect(fizzbuzz(3)).to.equal('fizz')
     expect(fizzbuzz(9)).to.equal('fizz')
+    fizz.isFizz.restore()
+    buzz.isBuzz.restore()
   })
 })
